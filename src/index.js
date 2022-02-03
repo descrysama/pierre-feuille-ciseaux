14c612caad81150ff342739e880a7e411ba4f75c
DescrySama <erwing2002@gmail.com>
2022-02-03 15:28:43 +0100

    n

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 
 
 let contentSelect = document.querySelector('.content');
-let controllerBoxSelect = document.querySelector('.controllerBox');
 let ImgPPC = ['/img/rock.png', '/img/paper.png', '/img/cisors.png'];
-let scoreLeft = 0; //Score du joueur
-let scoreRight = 0; //Score de l'IA
+let score1 = 0; //Score du joueur
+let score2 = 0; //Score de l'IA
+let randomchoice;
+let playerchoice;
 
-const randomChoice = (min, max) => {
-  return Math.random() * (max - min);
+// Choix aléatoire pour l'IA.
+const randomChoice = () => {
+  return Math.floor(Math.random()* 3);
 }
-
 // Fonction qui ajoute mes boutons de jeu.
 const controllerBox = () => {
     let createParentDiv = document.createElement('div');
@@ -21,7 +22,6 @@ const controllerBox = () => {
       let createP = document.createElement('p');
       createChildDiv.setAttribute('class', 'gamebuttons');
       createChildDiv.setAttribute('id', 'gamebutton'+[i]);
-      createChildDiv.setAttribute('type', 'submit');
       createP.textContent = ppc[i];
       createChildDiv.appendChild(createP);
       createParentDiv.appendChild(createChildDiv);
@@ -50,40 +50,63 @@ const gameInfos = () => {
       contentSelect.appendChild(createParentDiv)
     }
 }
-// fonction qui réaffiche les valeurs
+// fonction qui réaffiche les valeurs.
 const updateInfos = () => {
   let getScore1 = document.getElementById('score0');
   let getScore2 = document.getElementById('score1');
+  let getImg1 = document.getElementById('img0')
+  let getImg2 = document.getElementById('img1')
   getScore1.textContent = score1;
   getScore2.textContent = score2;
+  getImg1.src = ImgPPC[playerchoice]
+  getImg2.src = ImgPPC[randomchoice]
   
 }
-
 //fonction déclanchée par le bouton "JOUER" dans le HTML et qui lance le jeu.
 function beginGame() {
     document.getElementById('playButton').remove();
     gameInfos()
     controllerBox()
-    
+    //boucle qui permet de savoir sur quoi on clique.
+    for (let i = 0; i < 3; i++) {
+      let gameButtons = document.getElementById('gamebutton'+[i]);
+      gameButtons.onclick = () => {
+        randomchoice = randomChoice();
+        if (gameButtons.textContent == 'Pierre' && randomchoice == 1) {
+          score2++
+          playerchoice = 0;
+        } else if (gameButtons.textContent == 'Papier' && randomchoice == 2) {
+          score2++
+          playerchoice = 1;
+        } else if (gameButtons.textContent == 'Ciseaux' && randomchoice == 0) {
+          score2++
+          playerchoice = 2;
+        } else if (gameButtons.textContent == 'Pierre' && randomchoice == 0) {
+          console.log(randomchoice);
+          playerchoice = 0;
+        } else if (gameButtons.textContent == 'Papier' && randomchoice == 1) {
+          console.log(randomchoice);
+          playerchoice = 1;
+        } else if (gameButtons.textContent == 'Ciseaux' && randomchoice == 2) {
+          console.log(randomchoice);
+          playerchoice = 2;
+        } else {
+          score1++
+          if (gameButtons.textContent == 'Pierre') {
+            playerchoice = 0;
+          } else if (gameButtons.textContent == 'Papier') {
+            playerchoice = 1;
+          } else {
+            playerchoice = 2;
+          }
+        }
+
+        updateInfos()
+      }
+    }
 }
 
-contentSelect.addEventListener('submit', (event) => {
-  event.preventDefault()
 
-  for (let i = 0; i < 3; i++) {
-    let gameButtons = document.getElementById('gamebutton'+[i]);
-    gameButtons.onclick = () => {
-    if (gameButtons.textContent == 'Pierre') {
-      document.getElementById('img0').src = ImgPPC[0]
-      console.log('pierre');
-    } else if (gameButtons.textContent == 'Papier') {
-      console.log('papier');
-      document.getElementById('img0').src = ImgPPC[1]
-    } else {
-      document.getElementById('img0').src = ImgPPC[2]
-      console.log('ciseaux');
-    }
-  }
-  }
-})
+
+
 
